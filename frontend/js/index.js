7328d9ea2efb2453a3619c4945f8e513e8c66400
sbplat <71648843+sbplat@users.noreply.github.com>
2022-12-10 19:24:45 -0500

    refactor: add more comments

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,3 +1,4 @@
+const SERVER_URL = "http://localhost:5000/";
 var year, hash, length;
 var currentRow = -1;
 var input = "";
@@ -5,17 +6,18 @@ var input = "";
 function updateCellClasses(cellClasses, correct) {
     let row = $("#tiles tr").eq(currentRow);
     let cells = row.children();
+    let animation = correct ? "jump" : "animate";  // Different animations for correct/wrong answer.
     for (let i = 0; i < length; ++i) {
         setTimeout(function() {
-            cells.eq(i).removeClass();
-            cells.eq(i).addClass(correct ? "jump" : "animate");
+            cells.eq(i).removeClass();  // Reset the style (cell background colour).
+            cells.eq(i).addClass(animation);  // Animate the cell.
             if (cellClasses[i] === undefined) {
                 cells.eq(i).addClass("cell-incorrect");
             } else {
                 cells.eq(i).addClass(cellClasses[i]);
             }
             setTimeout(function() {
-                cells.eq(i).removeClass(correct ? "jump" : "animate");
+                cells.eq(i).removeClass(animation);  // Stop the correct/wrong answer animation.
             }, 500);
         }, 300);
     }
@@ -31,14 +33,14 @@ function displayWinMessage() {
     message += `<br><br>Reload the page to play again!`;
     setTimeout(function() {
         $("#center-box").addClass("content").html(message);
-    }, 1000);
+    }, 1000);  // Display the message box after 1s.
 }
 
 function checkGuess(hash, guess) {
     console.log("Checking guess", hash, guess);
     var result;
     $.ajax({
-        url: "http://localhost:5000/check",
+        url: SERVER_URL + "check",
         type: "POST",
         crossDomain: true,
         async: false,
@@ -52,13 +54,14 @@ function checkGuess(hash, guess) {
     let correct = result.correct;
     let correct_indexes = result.correct_indexes;
     let wrong_places = result.wrong_places;
-    let cellClasses = [...Array(length)];
+    let cellClasses = [...Array(length)];  // Create an empty array.
     for (let i = 0; i < correct_indexes.length; ++i) {
         cellClasses[correct_indexes[i]] = "cell-correct";
     }
     for (let i = 0; i < wrong_places.length; ++i) {
         cellClasses[wrong_places[i][0]] = "cell-wrong-place";
     }
+    // If cellClasses[i] is undefined, then its style is cell-incorrect.
     updateCellClasses(cellClasses, correct);
     if (correct) {
         displayWinMessage();
@@ -68,12 +71,12 @@ function checkGuess(hash, guess) {
 
 function addRow() {
     let table = $("#tiles");
-    let cell = $("<td></td>").addClass("cell-blank").text(" ");
+    let cell = $("<td></td>").addClass("cell-blank").text(" ");  // Create a blank cell.
     let row = $("<tr></tr>");
     for (let i = 0; i < length; ++i) {
-        row.append(cell.clone());
+        row.append(cell.clone());  // Add each cell to the row.
     }
-    table.append(row);
+    table.append(row);  // Add the new row to the table.
     ++currentRow;
 }
 
@@ -82,19 +85,19 @@ function updateInput() {
     let row = $("#tiles tr").eq(currentRow);
     let cells = row.children();
     for (let i = 0; i < input.length; ++i) {
-        cells.eq(i).text(input[i]);
+        cells.eq(i).text(input[i]);  // Set the cell to be the input char.
     }
     for (let i = input.length; i < length; ++i) {
-        cells.eq(i).text(" ");
+        cells.eq(i).text(" ");  // The rest of them are empty.
     }
 }
 
 $(document).ready(function() {
     console.log("ready!");
 
-    // Get the event
+    // Get the year, hash, and length from the server.
     $.ajax({
-        url: "http://localhost:5000/get_event",
+        url: SERVER_URL + "get_event",
         type: "GET",
         crossDomain: true,
         async: false,
@@ -109,29 +112,31 @@ $(document).ready(function() {
         }
     });
 
-    $("#year").text(`Year: ${year}`);
-
     console.log(year, hash, length);
 
-    addRow(); // Add the first row
+    $("#year").text(`Year: ${year}`);  // Display the year for the event.
+    addRow(); // Add the first row.
 
+    // Watch for the keydown events.
     document.addEventListener("keydown", function(event) {
         const key = event.key;
         if (key === "Backspace" || key === "Delete") {
-            input = input.slice(0, -1);
+            input = input.slice(0, -1);  // Delete the last char.
         } else if (key === "Enter") {
-            if (input.length === length) {
+            if (input.length === length) {  // Make sure all the cells are filled.
                 if (!checkGuess(hash, input)) {
-                    input = "";
-                    addRow();
+                    input = "";  // Reset the input.
+                    addRow();  // Create a new row.
                 }
             }
         } else {
+            // Make sure the input doesn't exceed the length of the event and also
+            // make sure that the input is a char, not another type of key (ctrl, etc.).
             if (input.length < length && key.length === 1) {
                 input += key.toLowerCase();
-                input = input.slice(0, length);
+                input = input.slice(0, length);  // If it exceeds the length, cut it.
             }
         }
-        updateInput();
+        updateInput();  // Update the input displayed on screen.
     });
 });
